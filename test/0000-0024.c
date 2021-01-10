fb6c500ae3e699d390308b2913da33774e0e6bdb
Brett Sheffield <brett@gladserv.com>
2021-01-10 17:55:25 +0100

    net_send() - test mapping valid source file

--- test/0000-0024.c
+++ test/0000-0024.c
@@ -7,14 +7,18 @@
 
 int main()
 {
-	char src[] = "./src_does_not_exist";
-	char *arg0[] = { src, NULL };
+	char invalid[] = "./src_does_not_exist";
+	char valid[] = "./0000-0024.c";
+	char *arg0[] = { invalid, NULL };
+	char *arg1[] = { valid, NULL };
 	char **argv = arg0;
 	int argc = (int) sizeof arg0 / sizeof arg0[0] - 1;
 
 	test_name("net_send()");
 
 	test_assert(net_send(&argc, argv) == -1, "net_send() - invalid source file");
+	argv = arg1;
+	test_assert(net_send(&argc, argv) == 0, "net_send() - valid source file");
 
 	return fails;
 }
