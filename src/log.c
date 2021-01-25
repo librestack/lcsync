d46fc34d388a273cdaf188bc4468b0fdaa20edc9
Brett Sheffield <brett@gladserv.com>
2021-01-25 18:17:17 +0100

    add logging module

---
+++ src/log.c
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * log.c
+ *
+ * this file is part of LIBRESTACK
+ *
+ * Copyright (c) 2012-2020 Brett Sheffield <bacs@librecast.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see the file COPYING in the distribution).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "log.h"
+
+#define LOG_BUFSIZE 128
+
+unsigned int loglevel = LOG_LOGLEVEL_DEFAULT;
+
+void logmsg(unsigned int level, const char *fmt, ...)
+{
+	int len;
+	char *mbuf = NULL;
+	char buf[LOG_BUFSIZE];
+	char *b = buf;
+	va_list argp;
+	if ((level & loglevel) != level) return;
+	va_start(argp, fmt);
+	len = vsnprintf(buf, LOG_BUFSIZE, fmt, argp);
+	if (len > LOG_BUFSIZE) {
+		/* need a bigger buffer, resort to malloc */
+		mbuf = malloc(len + 1);
+		va_end(argp);
+		va_start(argp, fmt);
+		vsprintf(mbuf, fmt, argp);
+		b = mbuf;
+	}
+	va_end(argp);
+	if (level == LOG_INFO)
+		fprintf(stdout, "%s\n", b);
+	else
+		fprintf(stderr, "%s\n", b);
+	free(mbuf);
+}
---
+++ src/log.h
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * log.h
+ *
+ * this file is part of LIBRESTACK
+ *
+ * Copyright (c) 2012-2021 Brett Sheffield <bacs@librecast.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see the file COPYING in the distribution).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LSD_LOG
+#define __LSD_LOG 1
+
+#define LOG_LEVELS(X) \
+	X(0,    LOG_NONE,       "none")                                 \
+	X(1,    LOG_SEVERE,     "severe")                               \
+	X(2,    LOG_ERROR,      "error")                                \
+	X(4,    LOG_WARNING,    "warning")                              \
+	X(8,    LOG_INFO,       "info")                                 \
+	X(16,   LOG_TRACE,      "trace")                                \
+	X(32,   LOG_FULLTRACE,  "fulltrace")                            \
+	X(64,   LOG_DEBUG,      "debug")
+#undef X
+
+#define LOG_ENUM(id, name, desc) name = id,
+enum {
+	LOG_LEVELS(LOG_ENUM)
+};
+
+#define LOG_LOGLEVEL_DEFAULT 15
+extern unsigned int loglevel;
+
+#define FMTV(iov) (int)(iov).iov_len, (const char *)(iov).iov_base
+#define LOG(lvl, fmt, ...) if ((lvl & loglevel) == lvl) logmsg(lvl, fmt ,##__VA_ARGS__)
+#define BREAK(lvl, fmt, ...) {LOG(lvl, fmt ,##__VA_ARGS__); break;}
+#define CONTINUE(lvl, fmt, ...) {LOG(lvl, fmt ,##__VA_ARGS__); continue;}
+#define DIE(fmt, ...) {LOG(LOG_SEVERE, fmt ,##__VA_ARGS__);  _exit(EXIT_FAILURE);}
+#define DEBUG(fmt, ...) LOG(LOG_DEBUG, fmt ,##__VA_ARGS__)
+#define ERROR(fmt, ...) LOG(LOG_ERROR, fmt ,##__VA_ARGS__)
+#define ERRMSG(err) {LOG(LOG_ERROR, err_msg(err));}
+#define FAIL(err) {LOG(LOG_ERROR, err_msg(err));  return err;}
+#define FAILMSG(err, fmt, ...) {LOG(LOG_ERROR, fmt ,##__VA_ARGS__);  return err;}
+#define INFO(fmt, ...) LOG(LOG_INFO, fmt ,##__VA_ARGS__)
+#define TRACE(fmt, ...) LOG(LOG_TRACE, fmt ,##__VA_ARGS__)
+
+void logmsg(unsigned int level, const char *fmt, ...)
+#ifdef __GNUC__
+	__attribute__((format(printf, 2 ,3)))
+#endif
+;
+
+#endif /* __LSD_LOG */
