8d0042d69f6888c9a5d7d3b31c65c79951cbb984
Brett Sheffield <brett@gladserv.com>
2021-01-25 22:28:31 +0100

    -q silences hexdump

--- src/mtree.c
+++ src/mtree.c
@@ -42,6 +42,7 @@ struct mtree_thread {
 void mtree_hexdump(mtree_tree *tree, FILE *fd)
 {
 	char hex[HEXLEN];
+	if (quiet) return;
 	for (size_t i = 0; i < tree->nodes; i++) {
 		sodium_bin2hex(hex, HEXLEN, tree->tree + i * HASHSIZE, HASHSIZE);
 		fprintf(fd, "%08zu: %.*s\n", i, HEXLEN, hex);
