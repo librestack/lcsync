cf25a489b951815d3202d3d90254f92513c262b9
Brett Sheffield <brett@gladserv.com>
2021-02-04 21:20:19 +0100

    add test 51

---
+++ test/0000-0051.c
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright (c) 2021 Brett Sheffield <bacs@librecast.net> */
+
+#include "test.h"
+#include "../src/mld.h"
+#include <librecast.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+int main(void)
+{
+	const int limit = 1; /* a modest number for normal test runs */
+	mld_t *mld;
+	struct in6_addr *addr[limit];
+	struct sockaddr_in6 *sad;
+	struct addrinfo *ai;
+	char channame[16] = "";
+	int t;
+	lc_ctx_t *lctx;
+	lc_channel_t *chan;
+	test_name("mld_filter_grp_add() / mld_filter_grp_cmp()");
+	lctx = lc_ctx_new();
+
+	mld = mld_init(1);
+	for (int i = 0; i < limit; i++) {
+		snprintf(channame, 16, "channel %i", i);
+		chan = lc_channel_new(lctx, channame);
+		ai = lc_channel_addrinfo(chan);
+		sad = (struct sockaddr_in6 *)ai->ai_addr;
+		addr[i] = &(sad->sin6_addr);
+		/* test false before adding */
+		test_log("testing '%s' (false)\n", channame);
+		test_assert(!mld_filter_grp_cmp(mld, 0, addr[i]),
+				"mld_filter_grp_cmp() - before adding (%i)", i);
+		test_log("adding '%s'\n", channame);
+		mld_filter_grp_add(mld, 0, addr[i]);
+		/* test positive after adding */
+		test_log("testing '%s' (true)\n", channame);
+		test_assert(mld_filter_grp_cmp(mld, 0, addr[i]),
+				"mld_filter_grp_cmp() - added (%i)", i);
+		/* ensure timer set */
+		t = mld_filter_timer_get(mld, 0, addr[i]);
+		test_assert(t == MLD_TIMEOUT, "timer set to %i", t);
+	}
+
+	/* timer ticks down */
+	t = mld_filter_timer_get(mld, 0, addr[42]);
+	usleep(2000);
+	test_assert(t < MLD_TIMEOUT, "timer ticking %i", t);
+
+	// TODO work through state machine
+	//
+	// TODO test cmp with source address
+
+	mld_free(mld);
+	lc_ctx_free(lctx);
+	return fails;
+}
