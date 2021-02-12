787dd482963d1a78d2af0ca9596f7917b68c7715
Brett Sheffield <brett@gladserv.com>
2021-02-12 13:55:16 +0100

    working on 0062

--- test/0000-0062.c
+++ test/0000-0062.c
@@ -2,7 +2,10 @@
 /* Copyright (c) 2021 Brett Sheffield <bacs@librecast.net> */
 
 #include "test.h"
+#include "../src/macro.h"
+#include "../src/log.h"
 #include "../src/mld.h"
+#include <assert.h>
 #include <librecast.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,52 +14,55 @@
 
 static void channel_part(lc_channel_t *chan)
 {
-	lc_channel_part(chan);
+	test_assert(!lc_channel_part(chan), "lc_channel_part()");
 	usleep(10000);
 }
 
 static void channel_join(lc_channel_t *chan)
 {
-	lc_channel_join(chan);
+	test_assert(!lc_channel_join(chan), "lc_channel_join()");
 	usleep(10000);
 }
 
 int main(void)
 {
-	const int limit = 64; /* a modest number for normal test runs */
+	const int limit = 1; /* a modest number for normal test runs */
 	mld_t *mld;
 	struct in6_addr *addr[limit];
-	struct sockaddr_in6 *sad;
-	struct addrinfo *ai;
 	char channame[16] = "";
 	lc_ctx_t *lctx;
 	lc_socket_t *sock[limit];
 	lc_channel_t *chan[limit];
-	int t;
+	int t = 0;
+
+	loginit();
 
 	test_name("MLD filter testing - joining via multicast");
-	lctx = lc_ctx_new();
-	mld = mld_start(NULL);
+	test_assert((lctx = lc_ctx_new()) != NULL, "lc_ctx_new()");
+	test_assert((mld = mld_start(NULL)) != NULL, "mld_start()");
 	for (int i = 0; i < limit; i++) {
 		snprintf(channame, 16, "channel %i", i);
 		sock[i] = lc_socket_new(lctx);
 		chan[i] = lc_channel_new(lctx, channame);
-		ai = lc_channel_addrinfo(chan[i]);
-		sad = (struct sockaddr_in6 *)ai->ai_addr;
-		addr[i] = &(sad->sin6_addr);
+		addr[i] = aitoin6(lc_channel_addrinfo(chan[i]));
+		assert(sock[i]); assert(chan[i]); assert(addr[i]);
+
 		/* test false before adding */
 		test_log("testing '%s' (false)\n", channame);
 		test_assert(!mld_filter_grp_cmp(mld, 0, addr[i]),
 				"mld_filter_grp_cmp() - before adding (%i)", i);
-		test_log("adding '%s'\n", channame);
 
-		lc_channel_bind(sock[i], chan[i]);
+		/* now join the channel */
+		test_log("adding '%s'\n", channame);
+		test_assert(!lc_channel_bind(sock[i], chan[i]), "lc_channel_bind()");
 		channel_join(chan[i]);
 
 		/* test positive after adding */
 		test_log("testing '%s' (true)\n", channame);
 		test_assert(mld_filter_grp_cmp(mld, 0, addr[i]),
 				"mld_filter_grp_cmp() - added (%i)", i);
+
+		/* and check the timer is set */
 		t = mld_filter_timer_get(mld, 0, addr[i]);
 		test_assert(t == MLD_TIMEOUT, "%i: timer set to %i", i, t);
 	}
@@ -65,7 +71,6 @@ int main(void)
 		test_assert(mld_filter_grp_cmp(mld, 0, addr[i]), "mld_filter_grp_cmp() - true (%i)", i);
 		/* remove group and check again */
 		channel_part(chan[i]);
-		// FIXME - FAIL mld_filter_grp_cmp() - false (1241374768) - overflow?
 		test_assert(!mld_filter_grp_cmp(mld, 0, addr[i]), "mld_filter_grp_cmp() - false (%i)", i);
 	}
 
