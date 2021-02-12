f49448b52996587d8ae9eda1964d396d409e95d1
Brett Sheffield <brett@gladserv.com>
2021-02-12 14:48:04 +0100

    be assert()ive

--- test/0000-0046.c
+++ test/0000-0046.c
@@ -8,6 +8,7 @@
 #include "../src/macro.h"
 #include "../src/mld.h"
 #include "../src/net.h"
+#include <assert.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <semaphore.h>
@@ -65,31 +66,36 @@ int main(void)
 	if (!mld) return fails;
 	test_log("%s() mld has address %p\n", "main", (void*)mld);
 	lctx = lc_ctx_new();
+	assert(lctx);
 	q = job_queue_create(2);
+	assert(q);
 
 	for (int i = 0; i < limit; i++) {
 		events = 0;
 		sock = lc_socket_new(lctx);
+		assert(sock);
 		chan[0] = lc_channel_new(lctx, "we will join this channel");
 		chan[1] = lc_channel_new(lctx, "but not this one");
+		assert(chan[0]);
+		assert(chan[1]);
 
 		job[0] = push_job(q, chan[0]);
 		job[1] = push_job(q, chan[1]);
 		usleep(1000);
 
-		lc_channel_bind(sock, chan[0]);
-		lc_channel_join(chan[0]);
+		test_assert(!lc_channel_bind(sock, chan[0]), "i: lc_channel_bind()", i);
+		test_assert(!lc_channel_join(chan[0]), "i: lc_channel_join()", i);
 		usleep(10000);
 
 		test_assert(!clock_gettime(CLOCK_REALTIME, &timeout), "clock_gettime()");
 		timeout.tv_sec += waits;
 
 		/* first job should return, we joined its channel */
-		test_assert(!sem_timedwait(&job[0]->done, &timeout), "timeout - mld_wait() channel 0");
+		test_assert(!sem_timedwait(&job[0]->done, &timeout), "%i: timeout - mld_wait() channel 0", i);
 		free(job[0]);
 
 		/* second channel will timeout, we ignored it */
-		test_assert(sem_timedwait(&job[1]->done, &timeout), "timeout - mld_wait() channel 1");
+		test_assert(sem_timedwait(&job[1]->done, &timeout), "%i: timeout - mld_wait() channel 1", i);
 		free(job[1]->arg);
 		free(job[1]);
 
@@ -97,7 +103,7 @@ int main(void)
 		lc_channel_free(chan[1]);
 		lc_channel_free(chan[0]);
 		lc_socket_close(sock);
-		test_assert(events == 1, "received %i/1 event notifications", events);
+		test_assert(events == 1, "%i: received %i/1 event notifications", i, events);
 	}
 	lc_ctx_free(lctx);
 	job_queue_destroy(q);
